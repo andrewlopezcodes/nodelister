bdf3483e0bc175165dadc7eab8de2ee554a69cd0
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-10 15:27:06 -0400

    index.js - requiring path, inside of the variable statPromises udated the return lstat to use path.join method with targetDir, filename as arguments.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const fs = require('fs');
 const util = require('util');
-const chalk = require('chalk')
+const chalk = require('chalk');
+const path = require('path');
 
 //method #2
 
@@ -14,13 +15,15 @@ const lstat = util.promisify(fs.lstat);
 //   lstat
 // } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err)
   }
 
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
