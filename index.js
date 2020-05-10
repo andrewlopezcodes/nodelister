9ea6073ae64d2b77149e829519c5792023193f9e
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-10 15:30:03 -0400

    added signatures

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ const util = require('util');
 const chalk = require('chalk');
 const path = require('path');
 
-//method #2
 
 const lstat = util.promisify(fs.lstat);
-
-// method #3
-
-// const {
-//   lstat
-// } = fs.promises;
 
 const targetDir = process.argv[2] || process.cwd();
 
@@ -30,28 +23,12 @@ fs.readdir(targetDir, async (err, filenames) => {
 
   for (let stats of allStats) {
     const index = allStats.indexOf(stats);
-
     if (stats.isFile()) {
       console.log(filenames[index])
     } else {
       console.log(chalk.red(filenames[index]))
     }
-
-    // console.log(filenames[index], stats.isFile());
-
-
   }
-
 });
 
-// //method #1
-// const lstat = filename => {
-//   return new Promise((resolve, reject) => {
-//     fs.lstat(filename, (err, stats) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(stats);
-//     })
-//   })
-// };
+// written by @andrewlopezcodes on Github and Instagram
